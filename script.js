da9f7585969f80cb0c88af1b7f93161e6091ebfe
Louis Coleman <coleloui18@gmail.com>
2022-01-10 12:05:54 -0800

    pulling

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
+var setText = document.getElementById('setup')
+var punchText = document.getElementById('punchline')
+var jokebtn = document.getElementById('get-joke')
+var punchbtn = document.getElementById('show-punchline')
+
 var punchLine = "";
 var setup = "";
 
 function handleGetJoke() {
+    punchText.setAttribute('class', 'hidden')
     fetch('http://manateejokesapi.herokuapp.com/manatees/random')
         .then(function(res) {
             return res.json();
         })
         .then(function(response) {
             setup = response.setup;
-            punchLine = response.punchLine;
+            punchLine = response.punchline;
+            punchText.textContent = punchLine
+            setText.textContent = setup
         })
 };
+
+function showPunchLine() {
+    punchText.setAttribute('class', 'show')
+}
+
+punchbtn.addEventListener('click', showPunchLine)
+jokebtn.addEventListener('click', handleGetJoke)
